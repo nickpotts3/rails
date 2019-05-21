2e2452d8afd136dc12a7d36f1d5f6d43eb083571
Nick Potts <nick.potts@NickPotts-MB2621.teamnorthwoods.com>
2019-05-21 16:49:23 -0400

    Links to each page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
 
 
   def index
-    @articles = Article.all?
+    @articles = Article.all
   end
 
   def new
@@ -19,8 +19,9 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  private
+    def article_params
+      params.require(:article).permit(:title, :text)
+    end
+
 end
-private
-  def article_params
-    params.require(:article).permit(:title, :text)
-  end
