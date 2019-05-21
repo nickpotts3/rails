cea05e95fc8e15848c97be92b45bee245a8ff587
Nick Potts <nick.potts@NickPotts-MB2621.local>
2019-05-21 19:38:17 -0400

    New schema to DB

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,8 +9,22 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+      if @article.update(article_params)
+        redirect_to @article
+      else
+        render 'edit'
+      end
+  end
+
   def create
     @article = Article.new(article_params)
+
     @article.save
     if @article.save
       redirect_to @article
@@ -22,6 +36,13 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def destroy
+  @article = Article.find(params[:id])
+  @article.destroy
+
+  redirect_to articles_path
+end
 
   private
     def article_params
